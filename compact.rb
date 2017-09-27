b073ed12213636703846c9dafdd556ee97f54c97
Nyxiis <appaxd@gmail.com>
2017-09-26 21:14:18 -0400

    Update compact.rb

--- compact.rb
+++ compact.rb
@@ -1,5 +1,5 @@
 w=File.readlines("wordslist.txt").sample.chomp.upcase;l=w.length;v=l
-while true
+while 1
     if(l==0)then puts w;exit end
     g=gets.upcase;s=(0..w.size-1).count{|i|g[i]==w[i]}
     if(s==v)then puts"W";exit;else l-=1;puts"#{s.to_s}/#{v.to_s}, #{l.to_s} left";end
